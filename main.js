35628f98f25ffb9d8c9f679411759ca43b81e5e4
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-07-14 21:05:12 -0300

    fetch avos

--- main.js
+++ main.js
@@ -1,40 +1,49 @@
+// ? Async/await
+async function fetchAvos() {
+  const response = await fetch('https://platzi-avo.vercel.app/api/avo')
+  const body = await response.json()
+  return body.data
+}
+const avocados = await fetchAvos()
+console.log(avocados)
+// top level await
 // ? Estado: fuente de la verdad
-const avocados = [
-  {
-    name: 'Maluma Hass',
-    price: 1.15,
-    image: 'https://platzi-avo.vercel.app/_next/image?url=%2Fimages%2Fmaluma.jpg&w=750&q=75'
-  },
-  {
-    name: 'Maluma Hass',
-    price: 1.15,
-    image: 'https://platzi-avo.vercel.app/_next/image?url=%2Fimages%2Fmaluma.jpg&w=750&q=75'
-  },
-  {
-    name: 'Maluma Hass',
-    price: 1.15,
-    image: 'https://platzi-avo.vercel.app/_next/image?url=%2Fimages%2Fmaluma.jpg&w=750&q=75'
-  },
-  {
-    name: 'Maluma Hass',
-    price: 1.15,
-    image: 'https://platzi-avo.vercel.app/_next/image?url=%2Fimages%2Fmaluma.jpg&w=750&q=75'
-  }
-]
+// const avocados = [
+//   {
+//     name: 'Maluma Hass',
+//     price: 1.15,
+//     image: 'https://platzi-avo.vercel.app/_next/image?url=%2Fimages%2Fmaluma.jpg&w=750&q=75'
+//   },
+//   {
+//     name: 'Maluma Hass',
+//     price: 1.15,
+//     image: 'https://platzi-avo.vercel.app/_next/image?url=%2Fimages%2Fmaluma.jpg&w=750&q=75'
+//   },
+//   {
+//     name: 'Maluma Hass',
+//     price: 1.15,
+//     image: 'https://platzi-avo.vercel.app/_next/image?url=%2Fimages%2Fmaluma.jpg&w=750&q=75'
+//   },
+//   {
+//     name: 'Maluma Hass',
+//     price: 1.15,
+//     image: 'https://platzi-avo.vercel.app/_next/image?url=%2Fimages%2Fmaluma.jpg&w=750&q=75'
+//   }
+// ]
 
 // ? Seleccionar los nodos del DOM que vamos a usar
 
 const avocadoList = document.querySelector('.section-1-list')
 
-function createAvoCard (palta) {
+function createAvoCard(palta) {
   const cardContainer = document.createElement('a')
   cardContainer.classList.add("cards")
   // ? cardContainer.href = ''
-  
+
   const cardImage = document.createElement('img')
-  cardImage.src = palta.image
+  cardImage.src = 'https://platzi-avo.vercel.app' + palta.image
   cardImage.alt = palta.name
-  
+
   const cardText = document.createElement('div')
   cardText.classList.add('card-text')
 
@@ -48,9 +57,11 @@ function createAvoCard (palta) {
 
   cardText.append(cardTextName, cardTextPrice)
   cardContainer.append(cardImage, cardText)
-  
+
   return cardContainer
 
 }
-const avocard = createAvoCard(avocados[3])
-avocadoList.append(avocard)
+for (const avocado of avocados) {
+  const avocard = createAvoCard(avocado)
+  avocadoList.append(avocard)
+}
